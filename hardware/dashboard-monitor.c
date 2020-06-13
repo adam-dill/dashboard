0ea4d9a4d66174b9db144f24923ae75db7bfe338
Adam Dill <adamtdill@gmail.com>
2020-06-12 20:15:40 -0500

    simulating a mouse move to wake the computer, then a click in order to focus chrome.

--- hardware/dashboard-monitor.c
+++ hardware/dashboard-monitor.c
@@ -37,10 +37,11 @@ int main(void) {
 
 void hdmi(int on) {
 	if (on && !hdmiState) {
-		system("/opt/vc/bin/tvservice -p && xdotool click 1");
+		system("tvservice -p");
+		system("xdotool mousemove 100 100 && xdotool click 1");
 		hdmiState = 1;
 	} else if (!on && hdmiState) {
-		system("/opt/vc/bin/tvservice -o");
+		system("tvservice -o");
 		hdmiState = 0;
 	}
 }
