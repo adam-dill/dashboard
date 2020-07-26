ec9f78cb579cb80c90e640d6d12f816b8f8c1b4d
Adam Dill <adamtdill@gmail.com>
2020-07-26 12:54:01 -0500

    Reducing the inputs and outputs.

--- hardware/dashboard-monitor.c
+++ hardware/dashboard-monitor.c
@@ -4,40 +4,23 @@
 #include <limits.h>
 #include <wiringPi.h>
 
-#define resetPin 0
 #define motionPin 1
-#define resetLedPin 2
 
 void hdmi(int);
 
 int hdmiState = 1;
 
 int main(void) {
-	int resetting = 0;
 	chdir("/home/pi/Documents/dashboard");
 	char cwd[PATH_MAX];
 	getcwd(cwd, sizeof(cwd));
 	
 	wiringPiSetup();
 	
-	pinMode(resetPin, INPUT);
-	pullUpDnControl(resetPin, PUD_DOWN);
 	pinMode(motionPin, INPUT);
 	pullUpDnControl(motionPin, PUD_DOWN);
-	pinMode(resetLedPin, OUTPUT);
-	digitalWrite(resetLedPin, LOW);
 	
 	while(1) {
-		if (resetting == 1) {
-			int currentResetLed = digitalRead(resetLedPin);
-			digitalWrite(resetLedPin, !currentResetLed);
-		}
-		if (resetting == 0 && digitalRead(resetPin) == HIGH) {
-			resetting = 1;
-			digitalWrite(resetLedPin, HIGH);
-			// TODO: notify in the display update is occuring.
-			system("npm run update");
-		}
 		int motionSensor = digitalRead(motionPin);
 		hdmi(motionSensor);
 		sleep(1);
