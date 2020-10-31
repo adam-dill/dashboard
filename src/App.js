1ea6598893740fa5cbe71eecb0b1ca4c177ec6ad
Adam Dill <adamtdill@gmail.com>
2020-10-31 06:04:22 -0500

    Move midnight train able the calendar in case the calendar runs long.

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ class App extends React.Component {
                     <div className="row">
                         <div className="col-6">
                             <Quote />
-                            <MiniCalendar />
                             <MidnightTrain />
+                            <MiniCalendar />
                         </div>
                         <div className="col-6">
                             <Headlines />
