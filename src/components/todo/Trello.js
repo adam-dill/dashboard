b3cdff57135205f064004a0527c339c397e44363
Adam Dill <adamtdill@gmail.com>
2020-11-09 04:53:27 -0600

    Restyle the todo list.

--- src/components/todo/Trello.js
+++ src/components/todo/Trello.js
@@ -34,12 +34,12 @@ class Trello extends React.Component {
 
     render() { 
         const items = this.state.todo.map((value, index) => {
-            return (<div key={index} className="py-3">{value}</div>);
+            return (<li key={index}>{value}</li>);
         });
         return (
             <div className="mt-5">
                 <div className="title">To-Do</div>
-                {items}
+                <ul className="todo-list">{items}</ul>
             </div>
         );
     }
