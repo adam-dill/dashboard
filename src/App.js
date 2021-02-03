81ce8a3b8f09ce3624da4619049f16c262a8608e
Adam Dill <adamtdill@gmail.com>
2021-02-03 07:20:32 -0600

    Removing midnighttrain while the service is down.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
                     <div className="row">
                         <div className="col-6">
                             <Quote />
-                            <MidnightTrain />
+                            {/*<MidnightTrain />*/}
                             <MiniCalendar />
                         </div>
                         <div className="col-6">
