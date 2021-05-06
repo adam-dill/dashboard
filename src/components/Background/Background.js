86a3f765f4bc95acbf51082ec6264b5b8d9a9d60
Adam Dill <adamtdill@gmail.com>
2021-05-06 05:41:51 -0500

    Updating background image to use the new external api.

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,63 +1,68 @@
 import React from 'react';
-import Image1 from '../../assets/images/wallpaper/1.jpg';
-import Image2 from '../../assets/images/wallpaper/2.jpg';
-import Image3 from '../../assets/images/wallpaper/3.jpg';
-import Image4 from '../../assets/images/wallpaper/4.jpg';
-import Image5 from '../../assets/images/wallpaper/5.jpg';
-import Image6 from '../../assets/images/wallpaper/6.jpg';
-import Image7 from '../../assets/images/wallpaper/7.jpg';
-import Image8 from '../../assets/images/wallpaper/8.jpg';
-import Image9 from '../../assets/images/wallpaper/9.jpg';
-import Image10 from '../../assets/images/wallpaper/10.jpg';
 
-const DELAY = 60000 * 10;
+const DELAY = 60000 * 1;
+
+const HOST = 'http://cityscape.adamdill.com';
+const ENDPOINT = `${HOST}/api/images?key=WNPW298-6W7M95X-JGA8XQR-XERQ9V8&group=1`;
 
 class Background extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            backgroundImage: Image1,
-            queueImage: undefined
+            currentIndex: 0,
+            backgrounds: [],
         }
     }
 
     componentDidMount() {
-        setInterval(this.setRandomImage.bind(this), DELAY)
+        setInterval(() => {
+            this.setState((prev) => {
+                return {currentIndex: prev.currentIndex + 1}
+            })
+        }, DELAY);
+        setInterval(() => this.hardReset(), 60000*30);
+        fetch(ENDPOINT)
+            .then(response => response.json())
+            .then(data => {
+                const backgrounds = this.shuffle(data.map(value => value.image_large));
+                this.setState({backgrounds});
+            });
     }
 
-    setRandomImage() {
-        const rand = Math.floor(Math.random() * 10) + 1;
-        let image = Image1;
-        switch(rand) {
-            case 1: image = Image1; break;
-            case 2: image = Image2; break;
-            case 3: image = Image3; break;
-            case 4: image = Image4; break;
-            case 5: image = Image5; break;
-            case 6: image = Image6; break;
-            case 7: image = Image7; break;
-            case 8: image = Image8; break;
-            case 9: image = Image9; break;
-            case 10: image = Image10; break;
+    hardReset = () => {
+        const backgrounds = this.shuffle(this.state.backgrounds);
+        this.setState({currentIndex: 0, backgrounds});
+    }
+
+    currentImage = () => {
+        const index = this.state.currentIndex % this.state.backgrounds.length;
+        return this.state.backgrounds[index];
+    }
+
+    shuffle = (arr) => {
+        let array = JSON.parse(JSON.stringify(arr));
+        let currentIndex = array.length, temporaryValue, randomIndex;
+        
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+        
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+        
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
         }
-        this.setState({queueImage: image});
-        setTimeout(() => {
-            this.setState({
-                backgroundImage: image,
-                queueImage: undefined
-            })
-        }, 2000);
+        
+        return array;
     }
 
     render() {
-        const style = this.state.queueImage ? 'fade-in' : '';
-        const queuedImage = (this.state.queueImage)
-            ? <img src={this.state.queueImage} className={`background-image vw-100 vh-100 ${style}`} />
-            : null;
         return (
             <div className="opacity-3 vw-100 vh-100 background-image">
-                <img src={this.state.backgroundImage} className="background-image vw-100 vh-100" />
-                { queuedImage }
+                <img src={this.currentImage()} className="background-image vw-100 vh-100" />
             </div>
         );
     }
