420d012edce920a2b5e2fa27cc07e1e58c560720
Adam Dill <adamtdill@gmail.com>
2022-03-20 18:20:19 -0400

    Primary time is now eastern

--- front-end/src/components/Clock.js
+++ front-end/src/components/Clock.js
@@ -7,12 +7,12 @@ const Clock = ({est, pst, cst, month, day, date}) => {
         <div className={`clock-container`}>
             <div className="current-time">
                 <div>
-                    {cst}
+                    {est}
                     <div className="date">{day}, {month.substr(0, 3)} {date}</div>
                 </div>
             </div>
             <div className="other-time">
-                <div><span className="mute">NY:</span> {est}</div>
+                <div><span className="mute">CHI:</span> {cst}</div>
                 <div className="seperator"></div>
                 <div><span className="mute">LA:</span> {pst}</div>
             </div>
